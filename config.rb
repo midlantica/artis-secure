145de01313296f7f5a2c1a27c21391d4deb2674b
Andrew Harper <druharper@gmail.com>
2015-02-25 15:44:28 -0600

    Gem Middleman deploy

--- config.rb
+++ config.rb
@@ -91,3 +91,15 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
+# https://github.com/karlfreeman/middleman-deploy
+# $ middleman deploy [--build-before]
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+
