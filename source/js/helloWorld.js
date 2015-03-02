b252ca57f821df37a56f79dba835f95814baeba3
Andrew Harper <druharper@gmail.com>
2015-03-02 12:37:06 -0600

    dark update

--- source/js/helloWorld.js
+++ source/js/helloWorld.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+var noun;
+var verb;
+var adjective;
+var story;
+
+noun = prompt("Give me a noun!");
+verb = prompt("Give me a verb!");
+adjective = prompt("Give me a adjective!");
+
+story = ("The " + noun + " was " + verb + " down the " + adjective + " street");
+
+document.write("<h2>" + story + "</h2>");
+
