889bac6cc316df01f82a65e30f41b1d5bd25af95
Andrew Harper <druharper@gmail.com>
2015-03-02 14:16:47 -0600

    Artis dark finish

--- config.rb
+++ config.rb
@@ -47,6 +47,31 @@ end
 #   end
 # end
 
+helpers do
+  # Calculate the years for a copyright
+  def copyright_years(start_year)
+    end_year = Date.today.year
+    if start_year == end_year
+      start_year.to_s
+    else
+      start_year.to_s + '-' + end_year.to_s
+    end
+  end
+
+  # Holder.js image placeholder helper
+  def img_holder(opts = {})
+    return "Missing Image Dimension(s)" unless opts[:width] && opts[:height]
+    return "Invalid Image Dimension(s)" unless opts[:width].to_s =~ /^\d+$/ && opts[:height].to_s =~ /^\d+$/
+
+    img  = "<img data-src=\"holder.js/#{opts[:width]}x#{opts[:height]}/auto"
+    img << "/#{opts[:bgcolor]}:#{opts[:fgcolor]}" if opts[:fgcolor] && opts[:bgcolor]
+    img << "/text:#{opts[:text].gsub(/'/,"\'")}" if opts[:text]
+    img << "\" width=\"#{opts[:width]}\" height=\"#{opts[:height]}\">"
+
+    img
+  end
+end
+
 set :sass_dir, 'css'
 set :scss_dir, 'css'
 set :css_dir, 'css'
